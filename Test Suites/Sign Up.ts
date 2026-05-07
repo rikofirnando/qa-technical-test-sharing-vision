2afa06409c186361dcec2a028a7d471b2756de7f
Riko Firnando <rikofirnando73@yahoo.com>
2026-05-07 08:44:49 +0700

    Penambahan Test Suite Sign Up

---
+++ Test Suites/Sign Up.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sign Up</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>65714a10-a359-47b2-9c4f-30819241a0b6</testSuiteGuid>
+</TestSuiteEntity>
