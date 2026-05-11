2655d3f8ccbfde261700db0a9e8e5fc77d10b95b
Riko Firnando <rikofirnando73@yahoo.com>
2026-05-11 16:41:21 +0700

    Update 2026-05-11

--- Scripts/Generate Report/Script1777916865936.groovy
+++ Scripts/Generate Report/Script1777916865936.groovy
@@ -1,4 +1,4 @@
 import denstoo.reporting.CSReport as CSReport
 
-CSReport.generate('/home/rikofirnando/Katalon Studio/QA Technical Test PT Sharing Vision - Riko Firnando/Reports/20260508_104358/Sign Up/20260508_104359/')
+CSReport.generate('/home/rikofirnando/Katalon Studio/QA Technical Test PT Sharing Vision - Riko Firnando/Reports/20260511_162053/DDT/Sign Up Form v2/20260511_162053/')
 
